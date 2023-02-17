96dfd4060e6736d3a31784caf44d26d0cac7eb28
Neider Ruiz <57574910+neiderruiz@users.noreply.github.com>
2023-02-16 22:27:06 -0500

    create module declarations

---
+++ modules/declarations.py
@@ -0,0 +1,14 @@
+#languages entry
+languages = [
+    ('es', 'español'),
+    ('en', 'inglés'),
+    ('pt', 'portugués'),
+    ('ar', 'árabe'),
+    ('zh', 'chino'),
+    ('hi', 'hindi')
+]
+#models openai
+models = ('tiny.en', 'tiny', 'base.en', 'base', 'small.en', 'small', 'medium.en', 'medium', 'large-v1', 'large-v2', 'large')
+
+# folder sounds
+FOLDER_SOUNDS = "./sounds/"
